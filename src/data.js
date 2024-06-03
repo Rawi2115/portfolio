6e6f6cef921902ae64fd180cc38d7646da650737
Rawi2115 <abdulhamidhaitham215@gmail.com>
2024-06-03 15:23:55 +0300

    new project and bg change
    added the microfied project and changed the background color for better visibility

--- src/data.js
+++ src/data.js
@@ -1,11 +1,20 @@
 const data = [
   {
+    projectUrl: "https://microfied.netlify.app",
+    projectName: "MicroFied",
+    description:
+      "Website for purchasing Arduinos,Raspberry pi and othe digital electronics",
+    tags: ["React", "React-Router"],
+    id: 1,
+    projectImg: "/microfied.jpg",
+  },
+  {
     projectUrl: "https://incandescent-tanuki-abbfef.netlify.app",
     projectName: "Van Life",
     description:
       "website for browsing and renting vans, you can login using the email:b@b.com password:p123 ",
     tags: ["React", "React-Router"],
-    id: 1,
+    id: 2,
     projectImg: "/vanlife.png",
   },
   {
@@ -13,7 +22,7 @@ const data = [
     projectName: "Quizzical",
     description: "Random Quizes website",
     tags: ["React"],
-    id: 2,
+    id: 3,
     projectImg: "/quizzical.png",
   },
   {
@@ -21,7 +30,7 @@ const data = [
     projectName: "Movie Watchlist",
     description: "Search,find and movies to your watchlist",
     tags: ["Vanilla Javascript"],
-    id: 3,
+    id: 4,
     projectImg: "/watchlist.png",
   },
 ];
