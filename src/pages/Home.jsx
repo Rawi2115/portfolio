de6d4022b731c75ef6223cb800be716c1b767003
Rawi2115 <abdulhamidhaitham215@gmail.com>
2024-07-22 11:03:13 +0300

    added nextjs to skills in portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { FaGithub, FaLinkedin, FaReact } from "react-icons/fa";
 import { SiReactrouter, SiTailwindcss, SiTypescript } from "react-icons/si";
 import { TabTitle } from "../data";
+import { RiNextjsFill } from "react-icons/ri";
 
 export default function Home() {
   TabTitle("Home");
@@ -11,16 +12,16 @@ export default function Home() {
       </h2>
       <div className="flex items-center md:flex-row md:gap-16 flex-col gap-6">
         <p className="pt-5 text-lg md:text-2xl md:max-w-md">
-          I'm Abdulhamid, a frontend developer with experties in React,React
-          Router,tailwind and typescript i also study computer engineering{" "}
+          I'm Abdulhamid, a frontend developer with experties in Nextjs ,React
+          ,React Router and tailwind i also study computer engineering{" "}
         </p>
         <div className="flex flex-col gap-10 items-center">
-          <FaReact className="text-cyan-600" size="80" />
+          <RiNextjsFill size="80" />
           <div className="flex gap-16">
+            <FaReact className="text-cyan-600" size="80" />
             <SiReactrouter className="text-red-700" size="80" />
-            <SiTailwindcss className="text-blue-400" size="80" />
           </div>
-          <SiTypescript size="60" className="text-blue-600" />
+          <SiTailwindcss className="text-blue-400" size="80" />
         </div>
       </div>
       <div className="flex justify-self-center md:justify-self-start items-center">
